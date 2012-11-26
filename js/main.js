e71d0af0203045839e8d9fce4bb7ce81a18db52b
Alex Turcu <talex@vt.edu>
2012-11-26 02:28:07 -0500

    simple multi-user suport

--- js/main.js
+++ js/main.js
@@ -1,6 +1,32 @@
 //require(["vendor/jquery-1.8.3.min", "vendor/jquery.vegas"], function() {
 
-    $(function() {
+    
+    
+oldHash = "";
+    
+// state changes
+(function(window,undefined){
+
+    // Prepare
+    var History = window.History; // Note: We are using a capital H instead of a lower h
+    if ( !History.enabled ) {
+         // History.js is disabled for this browser.
+         // This is because we can optionally choose to support HTML4 browsers or not.
+        return false;
+    }
+
+    // Bind to StateChange Event
+    History.Adapter.bind(window,'anchorchange',function(){ // Note: We are using statechange instead of popstate
+        hash = History.getHash(); // Note: We are using History.getState() instead of event.state
+        console.log(hash, oldHash);
+        if (hash == "") {
+            hash = "alex";
+        }
+        console.log(hash, oldHash);
+        if (hash != oldHash) {
+            oldHash = hash;
+            
+            $(function() {
         //$( "#tabs" ).tabs(); 
         // fix the classes
         $( ".tabs-bottom .ui-tabs-nav, .tabs-bottom .ui-tabs-nav > *" )
@@ -10,29 +36,13 @@
         $( ".tabs-bottom .ui-tabs-nav" ).appendTo( ".tabs-bottom" );
     })
 
-    $.ajax("data/alex.json",  {
+    dataName = window.location.hash.replace("#", "") || "alex";
+    $.ajax("data/"+dataName+".json",  {
         success: function(data) {
-            $("#info-box #name").text(data.name);
-            $("#info-box #headline").text(data.headline);
-            $("#info-box #bio").html(marked(data.bio.join("\n\n")));
-            links = ""
-            for (i=0; i<data.links.length; i++) {
-                link = data.links[i];
-                links = links + "<li>"+marked(link)+"</li>";
-            }
-            $("#info-box #links").html("<ul>"+links+"</ul>");
-            
-        },
-        dataType: "json",
-        error: function(jqXHR, textStatus, errorThrown) { 
-            alert( "error: "  + textStatus )
-        },
-    });
-
-
-    $( function() {
+            $( function() {
+      console.log(data["background"]);
       $.vegas({
-        src:'img/IMG_0062.JPG',
+        src:data["background"],
         fade:500,
         valign: '30%',
         loading: false,
@@ -67,3 +77,33 @@
         }
       });
     });
+    
+    
+    
+            $("#info-box #name").text(data.name);
+            $("#info-box #headline").text(data.headline);
+            $("#info-box #bio").html(marked(data.bio.join("\n\n")));
+            links = ""
+            for (i=0; i<data.links.length; i++) {
+                link = data.links[i];
+                links = links + "<li>"+marked(link)+"</li>";
+            }
+            $("#info-box #links").html("<ul>"+links+"</ul>");
+            
+        },
+        dataType: "json",
+        error: function(jqXHR, textStatus, errorThrown) { 
+            console.log( "error: "  + textStatus );
+        },
+    });
+
+            
+        }
+    });
+    
+    // first load
+    if (History.getHash() == "") {
+        History.pushState(null, null, "!#alex");
+    }
+
+})(window);
